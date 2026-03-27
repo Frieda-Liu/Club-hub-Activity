83192d26d8626b71d5b0c4331f9cf8d538891b42
frieda0315 <frieda1@ualberta.ca>
2026-03-27 15:23:24 -0400

    port

--- server/server.js
+++ server/server.js
@@ -42,5 +42,7 @@ app.use("/api/slot", slotRoutes);
 app.use("/api/members", memberRoutes);
 app.use("/api/signup", signupRoutes);
 
-const PORT = process.env.PORT || 3000 || 8080;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
